bd37a05ee2cb4df998237a88054f86fc56718e9c
Mariya Yermakova <windostre@gmail.com>
2023-06-03 12:43:19 +0300

    Main. Изменена инструкция и вынесена в отдельную переменную

--- src/main/java/gutu/qa/Main.java
+++ src/main/java/gutu/qa/Main.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Как к вам обращаться?");
         String welcomeMessage = "Привет";
+        String instructionMessage = "Введите ваше имя";
+        Scanner sc = new Scanner(System.in);
+        System.out.println(instructionMessage);
+
         String userName = sc.nextLine();
         System.out.println(welcomeMessage + "," + " " + userName + "!");
     }
